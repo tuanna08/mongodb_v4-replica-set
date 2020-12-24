ffc819a45fc40f63ccbb2fbeac3448e86086b53f
tuanna08 <52945640+tuanna08@users.noreply.github.com>
2020-12-24 16:54:08 +0700

    Update backup_mongodb.sh

--- backup_mongodb.sh
+++ backup_mongodb.sh
@@ -21,8 +21,8 @@ COLLECTION_NAME[1]="MgLead"
 # printf "Select case: \n(1): backup by collections.\n(2): backup all db.\n"
 # echo -n "Enter number:"
 # read selectcase
+mkdir -p $HOME/backup_mongodb/$DATE_STR/
 if [ "$SELECTCASE" = "backup_collections" ];then
-    mkdir -p $HOME/backup_mongodb/$DATE_STR/
     for i in "${COLLECTION_NAME[@]}"
     do     
         mongodump --host=$DB_HOST --username $USERNAME --password $PASSWORD --authenticationDatabase admin --collection=$i --db=$DB_NAME --gzip --out=$HOME/backup_mongodb/$DATE_STR/
