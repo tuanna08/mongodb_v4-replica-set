3602420a232b4c26d12503e38fde504049ce5bd4
tuanna08 <52945640+tuanna08@users.noreply.github.com>
2020-12-24 16:50:41 +0700

    Update backup_mongodb.sh

--- backup_mongodb.sh
+++ backup_mongodb.sh
@@ -3,6 +3,9 @@ echo "<=========================== Program by Nguyễn Anh Tuấn===============
 # backup mongodb
 #0 11 * * * bash $HOME/xxxxxxxxxxxxx.sh
 
+# SELECTCASE="backup_all"
+SELECTCASE="backup_collections"
+
 DATE_STR=$(date '+%d-%b-%Y')
 DATE_TIME_STR=$(date +"%d-%b-%YT%T")
 
@@ -15,11 +18,18 @@ PASSWORD=""
 COLLECTION_NAME[0]="MgEmployee"
 COLLECTION_NAME[1]="MgLead"
 
+# printf "Select case: \n(1): backup by collections.\n(2): backup all db.\n"
+# echo -n "Enter number:"
+# read selectcase
+if [ "$SELECTCASE" = "backup_collections" ];then
+    mkdir -p $HOME/backup_mongodb/$DATE_STR/
+    for i in "${COLLECTION_NAME[@]}"
+    do     
+        mongodump --host=$DB_HOST --username $USERNAME --password $PASSWORD --authenticationDatabase admin --collection=$i --db=$DB_NAME --gzip --out=$HOME/backup_mongodb/$DATE_STR/
+    done
+else
+    mongodump --host=$DB_HOST --username $USERNAME --password $PASSWORD --authenticationDatabase admin  --db=$DB_NAME --gzip --out=$HOME/backup_mongodb/$DATE_STR/
+fi
 
-mkdir -p $HOME/backup_mongodb/$DATE_STR/
-for i in "${COLLECTION_NAME[@]}"
-do     
-    mongodump --host=$DB_HOST --username $USERNAME --password $PASSWORD --authenticationDatabase admin --collection=$i --db=$DB_NAME --gzip --out=$HOME/backup_mongodb/$DATE_STR/
-done
 
 echo "Backup dữ liệu mongodb "$DB_NAME" thành công: "$DATE_TIME_STR >> $HOME/backup_mongodb/log_backup.out
